96629a5fd2d6eeaa4590355f86420f7cebcc29ae
Liryczny <wrzosek95@o2.pl>
2016-02-19 18:32:33 +0000

    Comments
    - Single Line Comments                   - //
    - Multi line Comments                     - /*   */
    - XML Documentation Comments  -  ///
    
    Comments are used to document what the program does and what specific
    blocks or lines of code do. C# compiler ignores comments.
    
    To Comment and Uncomment, there are 2 ways
    1. Use the designer
    2. Keyboard Shortcut: Ctrl+K, Ctrl+C, and Ctrl+K, Ctrl+U
    
    Note: Don't try to comment every line of code. Use comments only for
    blocks or lines of code that are difficult to understand

---
+++ Comments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+    class Comments
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            
+            /*
+            Console.WriteLine("Hello1");
+            Console.WriteLine("Hello2");
+            */
+            string City = "London";
+
+            //If city is london
+            if (City == "London")
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+
+/// <summary>
+/// This is a sample class and a sample documentation
+/// </summary>
+
+
