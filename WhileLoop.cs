4a67f6839368392a7797f13d0d66798fd1c1c8fc
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:05:52 +0000

    While Loop
    	1. While loop checks the condition first.
    	2. If the condition is true, statements within the loop are executed.
    	3. This process is repeated as long as the condition evaluates to true.
    
    Note: Don't forget to update the variable participating in the condition, so the loop can end, at some point.

---
+++ WhileLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ public class WhileLoop
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your target");
+            int target = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+            string output = "";
+            while (Start <= target)
+            {
+                output += Start + " ";
+                Start += 2;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
