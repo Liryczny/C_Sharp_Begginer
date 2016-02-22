7e84d89b80238090ed9662737eff95e2de1e0336
Liryczny <wrzosek95@o2.pl>
2016-02-22 13:38:41 +0000

    Built In Types
    Built-in types in C#:
    		○ Boolean type - Only true or false
    		○ Integral Types - sbyte, byte, short, ushort, int , uint, long, ulong, char
    		○ Floating Types - float and double
    		○ Decimal Types
    		○ String Types

---
+++ BuiltInTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class BuiltInTypes
+{
+   public static void Main(string[] args)
+   {
+       bool b = true;
+       int i = 0;
+       double d = 123.21312312;
+
+       //check Min and Max value for types
+       Console.WriteLine("Min = {0}", int.MinValue);
+       Console.WriteLine("Min = {0}", int.MaxValue);
+
+       Console.WriteLine(d);
+   }
+}
+
