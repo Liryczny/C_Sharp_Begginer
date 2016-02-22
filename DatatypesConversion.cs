661417b3b1bdc1b55da4d0769ad26c4265b4fd6b
Liryczny <wrzosek95@o2.pl>
2016-02-22 13:49:37 +0000

    Datatypes Conversion
    Implicit & Explicit conversion
    
    Implicit Conversion is done by the compiler
    	1. When there is no loss of information if the conversion is done
    	2. If there is no possibility of throwing exception during the conversion
    
    Example: Converting an int to a float will not lose any data and no exception will be thrown hence an implicit conversion can be done.
    
    Whereas when converting a float to an int, we lose the fractional part and also a possibility of overflow exception. Hence, in this case an explicit conversion is required. For explicit conversion we use cast operator the convert class in C#

---
+++ DatatypesConversion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class DatatypesConversion
+{
+   public static void Main(string [] args)
+    { 
+       //implicit conversion
+        int o = 100;
+        float d = o;
+
+        //explicit conversion
+        float f = 123.45F;
+        int i = (int)f;
+
+       //convert class
+        int j = Convert.ToInt32(f);
+        
+       //converting string
+        string strNumber = "155";
+        int s = int.Parse(strNumber);
+
+       //Try parse method
+        string num = "222TG";
+        int Result = 0;
+        bool IsConversionSuccessful = int.TryParse(num, out Result);
+
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(d);
+        Console.WriteLine(s);
+
+        if (IsConversionSuccessful)
+        {
+            Console.WriteLine(Result);
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+
+    }
+}
+
