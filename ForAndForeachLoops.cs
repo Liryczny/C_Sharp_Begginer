b753cac50fc00661614ca817037261a0c3b6ddee
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:10:11 +0000

    For & Foreach Loops
    The for loop
    A for loop is very similar to while loop. In a while loop we do the initialization at one place, condition check at another place, and modifying the variable at another place, whereas for loop has all of these at one place.
    
    The foreach loop
    A foreach loop is used to iterate through the items in a collection. For example, foreach loop can be used with arrays or collections such as ArrayList, HashTable and Generics

---
+++ ForAndForeachLoops.cs
@@ -0,0 +1,55 @@
+﻿using System;
+ public class ForAndForeachLoops
+    {
+        public static void Main(string[] args)
+        {
+            for (int d = 1; d <= 20; d++)
+            {
+                Console.WriteLine(d);
+                
+                if (d == 10)
+                    break;          
+            }
+            
+            Console.WriteLine();
+            
+            for (int e = 0; e <= 20; e++)
+            {
+                if (e % 2 == 1)
+                    continue;
+
+                Console.WriteLine(e);  
+            }
+            Console.WriteLine();
+
+            int[] Numbers = new int[3];
+            Numbers[0] = 101;
+            Numbers[1] = 102;
+            Numbers[2] = 103;
+
+            int[] Nums = { 201, 202, 203 };
+
+            foreach (int k in Numbers)
+            {
+                Console.WriteLine(k);
+            }
+            Console.WriteLine();
+
+            for (int j = 0; j < Nums.Length; j++)
+            {
+                Console.WriteLine(Nums[j]);
+            }
+
+            Console.WriteLine();
+
+            int i = 0;
+            while (i < Numbers.Length)
+            {
+                Console.WriteLine(Numbers[i]);
+                i++;
+            }
+
+
+
+        }
+    }
