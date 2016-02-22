c0edc16f64b9836ff6d5f3df8768dcee617d18da
Liryczny <wrzosek95@o2.pl>
2016-02-22 13:58:54 +0000

    Switch Statements

---
+++ SwitchStatements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class SwitchStatements
+    {
+        public static void Main(string[] args)
+        {
+            int TotalCoffeeCost = 0;
+
+            Start:
+            Console.WriteLine("Please select your coffee size: 1 - Small, 2 - Medium, 3 - Large");
+            int UserChoice = int.Parse(Console.ReadLine());
+
+            switch (UserChoice)
+            {
+                case 1:
+                    TotalCoffeeCost += 1;
+                    break;
+                case 2:
+                    TotalCoffeeCost += 2;
+                    break;
+                case 3:
+                    TotalCoffeeCost += 3;
+                    break;
+                default:
+                    Console.WriteLine("Your choice {0} is invalid", UserChoice);
+                    break;
+            }
+
+            Decide:
+            Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+            string UserDecision = Console.ReadLine();
+
+            switch (UserDecision.ToUpper())
+            {
+                case "YES":
+                    goto Start;
+                case "NO":
+                    break;
+                default:
+                    Console.WriteLine("Your choice {0} is invalid please try again...");
+                    goto Decide;
+            }
+
+            Console.WriteLine("Thank you for shopping with us");
+            Console.WriteLine("Bill Amount = {0} euro", TotalCoffeeCost);
+        }
+    }
