28a9f0e015bf54111913b1db922f94ccb6fa1246
Liryczny <wrzosek95@o2.pl>
2016-02-22 13:41:01 +0000

    String Types
    Verbatim Literal is a string with an @ symbol prefix, as in @"Hello".
    
    Verbatim literals make escape sequences translate as normal printable characters to enchance readability.
    
    Practical Example:
    Without Verbatim Literal : "C\\Hello\\World" - Less Readable
    With Verbatim Literal: @"C\Hello\World" - Better Readable

---
+++ StringType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class StringType
+    {
+        public static void Main(string [] args)
+        {
+            //escape sequences is backslash(\)
+            string s = "\"Michal\"";
+            string n = "One\nTwo\nThree";
+            string c = "C:\\hello\\world";
+
+            //Verbatim Literal
+            string d = @"C:\\hello\\world";
+
+            Console.WriteLine(s);
+            Console.WriteLine(n);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+
