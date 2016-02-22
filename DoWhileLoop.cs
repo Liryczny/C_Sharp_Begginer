3e7fb1ecd26a97e50b7dc67f3fb7f842814d5e58
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:07:50 +0000

    Do While Loop
    	1. A do loop checks its condition at the end of the loop.
    	2. This means that the do loop is guaranteed to execute at least one time.
    	3. Do loops are used to present a menu to the user.
    
    Difference - while & do while loop
    	1. While loop checks the condition at the beginning, whereas do while loop checks the condition at the end of the loop.
    	2. Do loop is guaranteed to execute at least once, whereas while loop is not.

---
+++ DoWhileLoop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ public class DoWhileLoop
+    {
+        public static void Main(string[] args)
+        {
+            string UserChoice = string.Empty;
+            do
+            {
+                Console.WriteLine("Please Enter your target?");
+                int UserTarget = int.Parse(Console.ReadLine());
+
+                int Start = 0;
+
+                while (Start <= UserTarget)
+                {
+                    Console.Write(Start + " ");
+                    Start += 2;
+                }
+
+            do
+            {
+                Console.WriteLine("Do you want to continue - Yes or No?");
+                UserChoice = Console.ReadLine().ToUpper();
+
+                if (UserChoice != "YES" && UserChoice != "NO")
+                {
+                     Console.WriteLine("Invalid Choice, please say Yes or No");
+                }
+
+             }while (UserChoice != "YES" && UserChoice != "NO");
+             } while (UserChoice == "YES");
+            
+        }
+    }
+
