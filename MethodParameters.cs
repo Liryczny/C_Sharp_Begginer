fe2a0826619ad87711cc2e4fb694b1cc37c61055
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:17:17 +0000

    Method Parameters
    Method parameter types
    
    There are 4 different types of parameters a method can have
    
    	1. Value Parameters: Creates a copy of the parameter passed, so modifications does not affect each other.
    	2. Reference Parameters: The ref method parameter keyword on a method parameter causes a method to refer to the same variable that was      passed into the method. Any changes made to the parameter in the method will be reflected in that variable when control passes back to the calling method.
    	3. Out Parameters: Use when you want a method to return more than one value
    	4. Parameter Arrays: The params keyword lets you specify a method parameter that

---
+++ MethodParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public class MethodParameters
+    {
+        public static void Main(string [] args)
+        {
+            int i = 0;
+            MethodParameters.SimpleMethod(ref i);
+            Console.WriteLine(i);
+            Console.WriteLine();
+
+            int Total = 0;
+            int Product = 0;
+            Calculate(10, 20, out Total, out Product);
+            Console.WriteLine("Sum = {0} && Product = {1}", Total, Product);
+            Console.WriteLine();
+
+            int[] Numbers = {101,102,103};
+            ParamsMethod();
+            ParamsMethod(Numbers);
+            ParamsMethod(1, 2, 3, 4, 5, 6);
+        }
+
+        public static void SimpleMethod(ref int j)
+        {
+            j = 101;
+        }
+
+        public static void Calculate(int FN, int SN, out int Sum, out int Product)
+        {
+                Sum = FN + SN;
+                Product = FN * SN;
+        }
+
+        public static void ParamsMethod(params int[] Numbers)
+        {
+            Console.WriteLine("There are {0} elements",Numbers.Length);
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+
