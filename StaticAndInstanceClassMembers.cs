6189dd16233a9536e6d67a5dc0250590ded6075b
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:45:49 +0000

    Static & Instance Class Members
    Static and Instance class Members
    
    When a class member includes a static modifier, the member is called as static member.
    When no static modifier is present the member is called as non static member or instance member.
    
    Static members are invoked using class name, whereas instance members are invoked using instances(objects) of the class.
    
    An instance member belongs to specific instance(object) of a class. If I create 3 objects of a class, I will have 3 sets of instance members in the memory, whereas there will ever be only copy of a static member, no matter how many instances of a class are created.
    
    Note: Class members = fields, methods, properties, events, indexers, constructors.

---
+++ StaticAndInstanceClassMembers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Circle
+{
+    static float _PI;
+    int _Radius;
+
+    static Circle()
+    {
+        Console.WriteLine("Static Constructor Called");
+        Circle._PI = 3.141F;
+    }
+
+    public Circle(int Radius)
+    {
+        Console.WriteLine("Instance Constructor Called");
+        this._Radius = Radius;
+    }
+
+    //behaviour
+    public float CalculateArea()
+    {
+        return this._Radius * Circle._PI * this._Radius;
+    }
+}
+   public class StaticAndInstanceClassMembers
+    {
+        public static void Main(string [] args)
+        {
+            Circle c1 = new Circle(5);
+            float Area1 = c1.CalculateArea();
+            Console.WriteLine("Area = {0}",Area1);
+
+            Circle c2 = new Circle(6);
+            float Area2 = c2.CalculateArea();
+            Console.WriteLine("Area = {0}", Area2);
+        }
+    }
+
