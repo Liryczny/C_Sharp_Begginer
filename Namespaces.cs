52f6a39ed45e259957467706d53c617a14a08ccf
Liryczny <wrzosek95@o2.pl>
2016-02-22 14:20:57 +0000

    Namespaces
    Why Namespaces
    
    Namespaces are used to organize your programs.
    
    They also provide assistance in avoiding name clashes.
    
    Namespaces
    Namespaces don't correspond to file, directory or assembly names. They could be written in separate files and /or separate assemblies and still belong to the same namespace.
    
     Namespaces can be nested in 2 ways
    
    Namespace alias directives. Sometimes you may encounter a long namespace and wish to have it shorter. This could improve readability and still avoid name clashes with similarly named methods

---
+++ Namespaces.cs
@@ -0,0 +1,45 @@
+﻿using System;
+//alias directives
+using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
+//Could use that
+//using System.Collections;
+//using System.Configuration;
+    public class Namespaces
+    {
+        public static void Main(string[] args)
+        {
+            PATA.ClassA.Print();
+            PATB.ClassA.Print();
+        }
+    }
+
+    namespace ProjectA
+    {
+        namespace TeamA
+        {
+            class ClassA
+            {
+                public static void Print()
+                {
+                    Console.WriteLine("Team A Print Method");
+                }
+            }
+        }
+    }
+
+    namespace ProjectA
+    {
+        namespace TeamB
+        {
+            class ClassA
+            {
+                public static void Print()
+                {
+                    Console.WriteLine("Team B Print Method");
+                }
+            }
+        }
+    }
+
