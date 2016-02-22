44062b4e87033c9af4e9e0c6e3b509897a239f82
Liryczny <wrzosek95@o2.pl>
2016-02-22 13:55:02 +0000

    Arrays
    An array is a collection of similar data types.
    
    Advantages: Arrays are strongly typed
    Disadvantages: Arrays cannot grow in size once initialized. Have to rely on integral indices to store or retrieve items from the array

---
+++ Arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Arrays
+{
+    public static void main(string[] args)
+    {
+        //first way of initializing
+        int[] evenNumbers = new int[3];
+
+        evenNumbers[0] = 0;
+        evenNumbers[1] = 2;
+        evenNumbers[2] = 4;
+
+        Console.WriteLine(evenNumbers[1]);
+        
+        //second way of initializing
+        int[] oddNumbers = {1, 3, 5};
+
+        Console.WriteLine(oddNumbers[1]);
+
+    }
+}
+
