e83045bff2648b641abd4a5663888373db8aeec9
Liryczny <wrzosek95@o2.pl>
2016-02-23 13:27:39 +0000

    Inheritance
    Why Inheritance
    Pillars of Object Oriented Programming
    	1. Inheritance
    	2. Encapsulation
    	3. Abstraction
    	4. Polymorphism
    	
    	1. Inheritance is one of the primary pillars of object oriented programming
    	2. It Allows code reuse
    	3. Code reuse can reduce time and errors
    Note: You will specify all the common fields, properties, methods in the base class, which allows reusability. In the derived class you will only have fields, properties and methods, that are specific to them.
    	1. In this example DerivedClass inherits from Parent Class.
    	2. C# supports only single class inheritance.
    	3. C# supports multiple interface inheritance.
    	4. Child class is a specialization of base class
    	5. Base classes are automatically instantiated before derived classes.
    	6. Parent Class constructor executes before child class constructor.

---
+++ Inheritance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class ParentClass
+{
+    public ParentClass()
+    {
+         Console.WriteLine("ParentClass Constructor called");
+    }
+
+    //overloading 
+    public ParentClass(string Message)
+    {
+        Console.WriteLine(Message);
+    }
+}
+
+public class ChildClass : ParentClass
+{
+    public ChildClass() : base("Derived Class controlling Parent class")
+    {
+        Console.WriteLine("ChildClass Constructor called");
+    }
+}
+
+    public class Inheritance
+    {
+        public static void Main(string [] args)
+        {
+            ChildClass CC = new ChildClass();
+        }
+    }
