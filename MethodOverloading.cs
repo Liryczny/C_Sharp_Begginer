1382455f682aa9dfe03e376e0494fb86e5e9ade3
Liryczny <wrzosek95@o2.pl>
2016-02-23 13:50:10 +0000

    Method Overloading
    Function overloading and Method overloading terms are used interchangeably.
    
    Method overloading allows a class to have multiple methods with the same, but, with a different signature. So, in C# functions can be overloaded based on the number, type(int,float etc) and kind(Value, Ref or Out) of parameters.
    
    The signature of a method consists of the name of the method and the type, kind(value, reference, or output) and the number of its formal parameters. The signature of a method does not include the return type and the params modifier. So, it is not possible to overload a function, just based on the return type of params modifier.

---
+++ MethodOverloading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+  public class MethodOverloading
+  {
+        public static void Main()
+        {
+            Add(123.4F, 1232.44F);
+            Add(123, 123);
+        }
+
+        public static void Add(int FN, int SN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+
+        public static void Add(float FN, float SN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+  }
+
