9f2ad330fc0e6d9146379d4d1bb454fc4842040a
Liryczny <wrzosek95@o2.pl>
2016-02-23 14:10:13 +0000

    Properties
    In C# to encapsulate and protect fields we use properties.
    
    	1. We use get and set accessors to implement properties
    	2. A property with both get and set accessor is a Read/Write property
    	3. A property with only get accessor is a Read only property
    	4. A property with only set accessor is a Write only property
    
    Note: The advantage of properties over traditional Get() and Set() methods is that, you can access them as if they were public fields

---
+++ Properties.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Student
+{
+    private int ID;
+    private string Name;
+    private int PassMark = 35;
+    public string City{get;set;}
+    public string Email{get;set;}
+    public int passMark
+    {
+        get
+        {
+            return this.PassMark;
+        }
+        
+    }
+    public string name
+    {
+        set{
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("Name cannot be null or empty");
+            }
+            this.Name = value;
+        }
+        get
+        {
+            //ternary operator
+            return string.IsNullOrEmpty(this.Name) ? "No name" : this.Name;
+        }
+       
+    }
+    public int Id
+    {
+        set{
+            if (value <= 0)
+            {
+                throw new Exception("Student ID cannot be negative");
+            }
+            this.ID = value;
+        }
+
+        get
+        {
+            return this.ID;
+        }
+        
+    }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student C1 = new Student();
+        C1.Id = 101;
+        C1.name = "Michal";
+
+        Console.WriteLine("Student ID = {0}", C1.Id);
+        Console.WriteLine("Student Name = {0}", C1.name);
+        Console.WriteLine("PassMark = {0}", C1.passMark);
+
+    }
+}
