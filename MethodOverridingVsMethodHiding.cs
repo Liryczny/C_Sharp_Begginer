d6d47057252dbda6f7c19f7fae2cf26ab383ea77
Liryczny <wrzosek95@o2.pl>
2016-02-23 13:46:05 +0000

    Method Overriding Vs Method Hiding
    In method overriding a base class reference variable pointing to a child class object, will invoke the overriden method in the Child class.
    In method hiding a base class reference variable pointing to a child class object, will invoke the hidden method in the Base class.

---
+++ MethodOverridingVsMethodHiding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class BaseClass
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("I am a Base Class Print Method");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public new void Print()
+    {
+        Console.WriteLine("I am a Derived Class Print Method");
+    }
+}
+    public class MethodOverridingVsMethodHiding
+    {
+        public static void Main(string [] args)
+        {
+            BaseClass B = new DerivedClass();
+            B.Print();
+
+            DerivedClass D = new DerivedClass();
+            D.Print();
+        } 
+    }
+
