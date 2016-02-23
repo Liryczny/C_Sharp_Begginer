8770133da10f97852ef9b06ce9463122fc801a86
Liryczny <wrzosek95@o2.pl>
2016-02-23 14:12:10 +0000

    Structs
    Just like classes structs can have
    	1. Private Fields
    	2. Public Properties
    	3. Constructors
    	4. Methods
    	
    Object initializer syntax, introduced in C# 3.0 can be used to initialize either a struct or a class.
    
    Note: There are several differences between classes and structs which we will be looking at in a later session.

---
+++ Structs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public struct Customer
+{
+    private int _id;
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int ID
+    {
+        get { return this._id; }
+        set { this._id = value; }
+    }
+    public Customer(int Id, string Name)
+    {
+        this._id = Id;
+        this._name = Name;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("ID = {0} && Name = {1}", _id, _name);
+    }
+}
+    public class Structs
+    {
+        public static void Main(string[] args)
+        {
+            Customer C1 = new Customer(1001, "William");
+            C1.PrintDetails();
+
+            Customer C2 = new Customer();
+            C2.ID = 1002;
+            C2.Name = "John";
+            C2.PrintDetails();
+
+            Customer C3 = new Customer{
+                ID = 103,
+                Name = "Rob"
+            };
+
+            C3.PrintDetails();
+        }
+    }
+
