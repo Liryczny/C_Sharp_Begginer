b3ac269c8168c519e53bdb3d4ee76e71ddbdfa3d
Liryczny <wrzosek95@o2.pl>
2016-02-23 13:33:16 +0000

    Method Hiding
    Inheritance Concepts
    
    Use the new keyword to hide a base class member. You will get a compiler warning, if you miss the new keyword.
    
    Different ways to invoke a hidden base class member from derived class.
    	1. Use base keyword.
    	2. Cast child type to parent type and invoke the hidden member
    	3. ParentClass PC  = new ChildClass();
    	PC.HiddenMethod();

---
+++ MethodHiding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Employee
+{
+    public string FirstName;
+    public string LastName;
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+public class PartTimeEmployee : Employee
+{
+    public new void PrintFullName()
+    {
+        //base.PrintFullName();
+        Console.WriteLine(FirstName + " " + LastName + " - Contractor");
+    }
+}
+
+public class FullTimeEmployee : Employee
+{
+
+}
+public class MethodHiding
+    {
+        public static void Main(string[] args)
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "FullTime";
+            FTE.LastName = "Employee";
+            FTE.PrintFullName();
+
+            Employee PTE = new PartTimeEmployee();
+            //PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.FirstName = "PartTime";
+            PTE.LastName = "Employee";
+            //((Employee)PTE).PrintFullName();
+            PTE.PrintFullName();
+            
+        }
+    }
+
