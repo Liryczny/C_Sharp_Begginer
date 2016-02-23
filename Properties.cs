dfb54e933591fd0c955719a27670f42148134cda
Liryczny <wrzosek95@o2.pl>
2016-02-23 14:00:21 +0000

    Properties
    Why Properties?
    Marking the class fields public and exposing to the external world is bad, as you will not have control over what gets assigned and returned
    *Problems with Public Fields
    1.Id should always be non negative number
    2.Name cannot be set to NULL
    3.If Student Name is missing "No Name" should be returned
    4.PassMark should be read only.
    
    Getter and Setter Methods
    Programming languages that does not have properties use getter and setter methods to encapsulate and protect fields.
    
    Note: Encapsulation is one of the primary pillars of object oriented programming

---
+++ Properties.cs
@@ -0,0 +1,54 @@
+﻿using System;
+public class Student
+{
+    private int ID;
+    private string Name;
+    private int PassMark = 35;
+
+    public int GetPassMark()
+    {
+        return this.PassMark;
+    }
+    public void SetName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new Exception("Name cannot be null or empty");
+        }
+        this.Name = name;
+    }
+
+    public string GetName()
+    {
+        //Ternary operator
+        return string.IsNullOrEmpty(this.Name) ? "No name" : this.Name;
+    }
+
+    public void SetID(int Id)
+    {
+        if (Id <= 0)
+        {
+            throw new Exception("Student ID cannot be negative");
+        }
+        this.ID = Id;
+    }
+    public int GetID()
+    {
+        return this.ID;
+    }
+
+}
+    public class Properties
+    {
+        public static void Main(string[] args)
+        {
+            Student C1 = new Student();
+            C1.SetID(103131);
+            C1.SetName("John");
+            
+            Console.WriteLine("Student ID = {0}", C1.GetID());
+            Console.WriteLine("Student Name = {0}", C1.GetName());
+            Console.WriteLine("PassMark = {0}", C1.GetPassMark());
+
+        }
+    }
