7ede788978f34d28f8511a48db62d87ca4fdcfa0
Liryczny <wrzosek95@o2.pl>
2016-02-23 14:19:39 +0000

    Interfaces
    We create interfaces using interface keyword. Just like classes interfaces also contains properties, methods, delegates or events, but only declarations and no implementations.
    It is a compile time error to provide implementations for any interface member.
    Interface members are public by default, and they don't allow explicit access modifiers.
    Interfaces cannot contain fields.
    If a class or a struct inherits from an interface, it must provide implementation for all interface members, Otherwise , we get compiler error.
    A class or a struct can inherit from more than one interface at the same time, but whereas, a class cannot inherit from more than once class at the same time.
    Interfaces can inherit from other interfaces. A class that inherits this interface must provide implementation for all interface members in the entire interface inheritance chain.
    We cannot create an instance of an interface, but an interface reference variable can point to a derived class object.

---
+++ Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface ICustomer1
+{
+    void Print();    
+}
+
+interface ICustomer2 : ICustomer1
+{
+    void Print2();
+}
+public class Customer : ICustomer2
+{
+    public void Print()
+    {
+        Console.WriteLine("Interface Print Method");
+    }
+    public void Print2()
+    {
+        Console.WriteLine("Interface 2 Print Method");
+    }
+}
+public class Interfaces
+{
+   public static void Main(string[] args)
+   {
+       Customer C1 = new Customer();
+       C1.Print();
+       C1.Print2();
+   }
+}
+
